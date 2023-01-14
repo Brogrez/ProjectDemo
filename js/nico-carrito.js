5b3ad2e403e0bb7ac88fc06f9b3a8879037daf06
nico <nico.mv1998@gmail.com>
2023-01-13 22:48:52 -0300

    terminando carrito de compras

--- js/nico-carrito.js
+++ js/nico-carrito.js
@@ -2,7 +2,8 @@
 
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito")
 const listaProductos = document.querySelector("#lista-cursos")
-
+let carrito = []
+const listaCarrito  = document.querySelector("#lista-carrito tbody")
 
 // listeners 
 cargaListeners()
@@ -19,25 +20,100 @@ function obtenerElemento(e){
     
     e.preventDefault()
     if(e.target.classList.contains("agregar-carrito")){
-        console.log(e.target.parentElement.parentElement)
+        // console.log(e.target.parentElement.parentElement)
         construirObjetoProducto(e.target.parentElement.parentElement)
-        // let nombreProducto = e.target.parentElement.parentElement.querySelector("h4")
-        
     }
-    
 }
 
 function construirObjetoProducto(producto){
-    let nombreProducto = producto.querySelector("h4")
-
-    const objCurso ={
-
+    // let nombreProducto = producto.querySelector("h4")
+    // let imagenProducto = producto.querySelector(".imagen-curso")
+    // let precioProducto = producto.querySelector(".precio")
+    
+    const objCurso = {
+        imagen: producto.querySelector(".imagen-curso").src,
+        nombre: producto.querySelector("h4").textContent,
+        precio: producto.querySelector(".precio span").textContent,
+        id: producto.querySelector("a").getAttribute("data-id"),
+        cantidad: 1
     }
-    console.log(nombreProducto)
-    console.log(producto, "producto")
+
+    let repetido = carrito.filter(e => e.id === objCurso.id)
+    if(repetido.length !== 0){
+        // repetido[0].cantidad++
+        // console.log(repetido)
+        // // repetido = [...repetido,carrito]
+        // carrito = [...repetido, carrito]
+        carrito = carrito.map(e => {
+            if(e.id === objCurso.id){
+                e.cantidad++
+                return e
+            }else{
+                return e
+            } 
+        })
+        
+        // carrito = resultado
+
+    }else{
+        carrito = [...carrito, objCurso]
+    }
+
+    console.log(repetido, "psuh")
+    // console.log(carrito,"inicio")
+    // if(carrito.length === 0){
+    //     carrito.push(objCurso)
+        
+    // }
+
+    // carrito.forEach(e => {
+    //     if(e.id === objCurso.id){
+    //         objCurso.cantidad++
+    //     }else{
+    //         carrito = [...carrito, objCurso]
+    //     }
+    // })
+    // console.log(carrito,"final")
+    
+    
+
+    // console.log(carrito)
+    console.log(carrito,"carrito")
+    plasmarCarrito(carrito)
+
+    // console.log(objCurso)
+    // console.log(producto, "producto")
+}
+
+function plasmarCarrito(carrito){
+
+    vaciarCarrito()
+
+
+
+    carrito.forEach(e => {
+        let elementTr = document.createElement("tr")
+
+        elementTr.innerHTML = `
+            <td> <img src="${e.imagen}"> </td>        
+            <td> ${e.nombre} </td>        
+            <td> ${e.precio} </td>        
+            <td> ${e.cantidad} </td>        
+            <td> <a class="borrar-curso" data-id="${e.id}"> X </a> </td>        
+        `
+        listaCarrito.appendChild(elementTr)
+
+
+    });
+
+    // console.log(carrito)
     
 }
 
 function vaciarCarrito(){
-    console.log("vaciando el carrito")
+
+    listaCarrito.innerHTML = ``
+
+    // console.log("vaciando el carrito")
 }
+
