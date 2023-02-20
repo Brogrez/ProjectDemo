193b992b082413bec68ed925ffac40983085034a
nico <nico.mv1998@gmail.com>
2023-02-20 00:05:10 -0300

    probando conflictos

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+let array = [['a','b','c'],[1,2,3,4],'salmon','pasa',5,6,7,8]
+array.forEach(element =>{//1,2,3
+    console.log(element)  
+    console.log(Array.isArray(element))
+    if(Array.isArray(element)){
+      element.forEach(element1 =>console.log(element1))//1   
+    }    
+})
